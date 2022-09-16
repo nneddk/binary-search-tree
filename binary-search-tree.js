cc064da6817e33a67cfa158a35d16bc854191e5d
nneddk <meljandre.mc@gmail.com>
2022-09-16 16:18:34 +0800

    added delete, add and find functions

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -9,14 +9,69 @@ function node(data){
 }
 class tree{
     constructor(root){
+        this.array = root;
         this.root = buildTree(root);
+
+
     }
+    print(fNode){
+        function prettyPrint(node, fNode, prefix = '', isLeft = true){
+            if (node.right !== null) {
+              prettyPrint(node.right, fNode, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+            }
+            console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data==fNode?'['+node.data+']':''+node.data}`);
+            if (node.left !== null) {
+              prettyPrint(node.left, fNode, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+            }
+          }
+        prettyPrint(this.root, fNode);
+    }
+    printArray(){
+        //prints array containing tree values, **it is not sorted, it gets sorted when placed inside the tree** 
+        //for debug purposes only
+        console.log(this.array);
+    }
+    insert(v){
+        //this rebuilds the entire tree SO it can be so much faster, but this works
+        this.array.push(v)
+        this.root = buildTree(this.array);
+    }
+    delete(v){
+        if (this.array.indexOf(v) > -1){
+            this.array.splice(this.array.indexOf(v), 1);
+            this.root = buildTree(this.array);
+        }
+    }
+    
+    find(v){
+        this.print(v);
+        /*
+        function finder(root, v){
+            //apologies for the if monster, but this should work
+            if(root.data == v){
+                return console.log('node found at: ', root);
+            }
+            if (root.left == null||root.right == null){
+                return;
+            }
+            if (v < root.data){
+                finder(root.left, v);
+            }else if(v > root.data){
+                finder(root.right, v);
+            }
+            
+        }
+        finder(this.root, v);
+        */
+    }
+
+
 };
 
-function buildTree(unsortedArray){
-    
+function buildTree(unsortedArray){    
     //probably better ways to do this but converting the array into the set removes duplicates.
     //might change and improve later on
+
     let sortedArray = [...new Set(unsortedArray)].sort(function(a, b){return a - b});
     //creates the tree 
     function treeMaker(arr, start, end){
@@ -35,41 +90,9 @@ function buildTree(unsortedArray){
     
     return root;
 }
-/* A function that constructs Balanced Binary Search Tree 
- from a sorted array 
- function sortedArrayToBST(arr, start, end)
- {
-     // Base Case 
-     if (start > end)
-     {
-         return null;
-     }
-     // Get the middle element and make it root 
-     var mid = parseInt((start + end + 1) / 2);
-     var n = node(arr[mid]);
-     // Recursively construct the left subtree and make it
-     // left child of root 
-     n.left = sortedArrayToBST(arr, start, mid - 1);
-     // Recursively construct the right subtree and make it
-     // right child of root 
-     n.right = sortedArrayToBST(arr, mid + 1, end);
-     return n;
- }
-*/
- 
-const numbers = [9, 5, 1, 7, 2, 12, 8, 4, 3, 11];
-buildTree(numbers);
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-  }
-prettyPrint(buildTree(numbers));
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+let example = new tree(numbers);
 
 function preOrder(node)
 {
@@ -81,5 +104,3 @@ function preOrder(node)
     preOrder(node.left);
     preOrder(node.right);
 }
-
-preOrder(buildTree(numbers));
